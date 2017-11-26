019b1dab4fe60bbc87f88674fb06104483127474
nh95 <31866573+nh95@users.noreply.github.com>
2017-11-26 18:14:01 -0500

    Update todos.php

--- todos.php
+++ todos.php
@@ -2,5 +2,24 @@
 
 class todos extends collection {
 protected static $modelName = 'todo';
+  
+  public static function insertData(){
+        $conn = dbConn::getConnection();
+        $tableName = get_called_class();
+        $sql = 'INSERT INTO ' . $tableName. '   (owneremail,ownerid,createddate,duedate,message,isdone) VALUES
+                        (?,?,?,?,?,?)';
+        try{
+        $conn->beginTransaction();
+        $stmt = $conn -> prepare($sql);
+        $stmt->execute(array("naveen@123test",1,"2017-09-11 00:00:00","2017-09-11 00:00:00","",0));
+        print $conn->lastInsertId();
+        $conn-> commit();
+        } catch(PDOExecption $e) {
+        $conn->rollback();
+        print "Error!: " . $e->getMessage() . "</br>";
+        }
+   }
+}
+
 
 ?>
