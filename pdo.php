72346affda5f21ce1874fcfaed23587c295882b1
nh95 <31866573+nh95@users.noreply.github.com>
2017-11-26 18:57:57 -0500

    changes

--- pdo.php
+++ pdo.php
@@ -9,23 +9,36 @@ define('USERNAME', 'nh95');
 define('PASSWORD', '1994apache');
 define('CONNECTION', 'sql1.njit.edu');
 
+//turn on debugging messages
+ini_set('display_errors', 'true');
+error_reporting(E_ALL);
+//instantiate the program object
+//Class to load classes it finds the file when the progrm starts to fail for calling a missing class
+class Manage {
+    public static function autoload($class) {
+                //you can put any file name or directory here
+	include strtolower($class) . '.php';
+	}			 	
+}
+spl_autoload_register(array('Manage', 'autoload'));
 
-
-
-
-
-
-
+//For Accounts
+$records = accounts :: findAll();
+echo '<h1> All records from accounts </h1>';
+Displaytable::display($records);
 
 echo '<h1> Select from account id=1 </h1>';
-$records = accounts :: getById(1);
-DisplayResult::displayTable($records);
+$records = accounts :: findOne(1);
+Displaytable::displayTable($records);
 
-echo '<h1>select all from accounts</h1>';
-$records = accounts::findAll();
-DisplayResult::displayTable($records);
-echo '<h1> insert into todos </h1>';
-todos :: insertData();
 
+//For todos
+$records = todos :: findAll();
+echo '<h1> All records from todos </h1>';
+Displaytable::display($records);
+
+echo '<h1> Select from todos id=1 </h1>';
+$records = accounts :: findOne(1);
+Displaytable::display($records);
 
 ?>
