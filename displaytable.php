1f3259aaea3c4029b084bc4cf1b18559936d1fb5
nh95 <31866573+nh95@users.noreply.github.com>
2017-11-26 18:16:16 -0500

    Create displaytable.php

---
+++ displaytable.php
@@ -0,0 +1,29 @@
+<?php
+class DisplayResult{
+
+
+  public static function displayTable($records){
+  $html = "<html><body><table border = 1>";
+  $html .= "<tr>";
+  //Set the header using the 1st record
+  foreach($records[0] as $key => $value){
+  $html .="<th>" .$key. "</th>";
+  }
+  $html .= "</tr>";
+  //generate rows
+  foreach($records as $record){
+  $html .= "<tr>";
+  foreach ($record as $key => $value){
+  $html .="<td>" .$value. "</td>";
+  }
+  $html .= "</tr>";
+  }
+  $html .="</table>";
+  echo $html;
+  }
+
+}
+
+
+
+?>
