e64146549d6497e5c57aaf8d759f9f8868195a8b
nh95 <31866573+nh95@users.noreply.github.com>
2017-11-26 19:34:20 -0500

    modified save method

--- todos.php
+++ todos.php
@@ -3,22 +3,47 @@
 class todos extends collection {
 protected static $modelName = 'todo';
   
-  public static function insertData(){
-        $conn = dbConn::getConnection();
-        $tableName = get_called_class();
-        $sql = 'INSERT INTO ' . $tableName. '   (owneremail,ownerid,createddate,duedate,message,isdone) VALUES
-                        (?,?,?,?,?,?)';
-        try{
-        $conn->beginTransaction();
-        $stmt = $conn -> prepare($sql);
-        $stmt->execute(array("naveen@123test",1,"2017-09-11 00:00:00","2017-09-11 00:00:00","",0));
-        print $conn->lastInsertId();
-        $conn-> commit();
-        } catch(PDOExecption $e) {
-        $conn->rollback();
-        print "Error!: " . $e->getMessage() . "</br>";
-        }
-   }
+  static public function save($todo){
+  	  $conn = dbConnection::getConnection();
+   	  if(isset($todo->id)){
+  	    $sql = self :: getUpdateQuery();
+  	  }else {
+ 	    $sql = self :: getInsertQuery();
+  	  }
+  	  try{
+  	    $conn->beginTransaction();  
+  	    $update = $conn -> prepare($sql);
+  	    if(isset($todo->id)){
+  	    	$update -> bindValue (':id', $todo->id);
+  	    }
+ 	    $update -> bindValue(':owneremail', $todo->owneremail);
+	    $update -> bindValue(':ownerid', $todo->ownerid);
+	    $update -> bindValue(':createddate', $todo->createddate);
+	    $update -> bindValue(':duedate', $todo->duedate);
+  	  $update -> bindValue(':message', $todo->message);
+	    $update -> bindValue(':isdone',$todo->isdone);
+  	  $update -> execute();
+ 	    $lastId = $conn->lastInsertId(); 
+  	    $conn-> commit();
+ 	    return $lastId;
+  	  }
+  	  catch(PDOExecption $e) {
+  	  $conn->rollback();	
+
+  	}
+  
+  static public function getUpdateQuery(){
+  	 $tableName = get_called_class();
+  	 $sql = 'UPDATE ' . $tableName. '  set  owneremail = :owneremail,ownerid = :ownerid,createddate=:createddate,duedate=:duedate,message= :message,isdone=:isdone  where id = :id';
+  	 return sql;
+  }
+  	
+  static public function getInsertQuery(){
+  	$tableName = get_called_class();
+    $sql = 'INSERT INTO ' . $tableName. '   (owneremail,ownerid,createddate,duedate,message,isdone) VALUES
+    	   (:owneremail,:ownerid,:createddate,:duedate,:message,:isdone)';
+    return sql;
+  }
 }
 
 
